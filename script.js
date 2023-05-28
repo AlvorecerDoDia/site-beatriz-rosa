fc214dbc0f5633c8ed4347304c9ac69139ada188
Mikael De Jesus <mikaeldejesus444@gmail.com>
2023-05-28 14:42:24 -0300

    att

---
+++ script.js
@@ -0,0 +1,9 @@
+function clickMenu(){
+    const Menu = document.getElementById('menu')
+    if(Menu.style.display == 'none'){
+        Menu.style.display = 'block'
+    }else{
+        Menu.style.display = 'none'
+    }
+    
+}
